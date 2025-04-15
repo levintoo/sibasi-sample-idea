ede309df0320553751da3c4445366ff7f66523ea
levintoo <tooklevin@gmail.com>
2025-04-15 19:24:28 +0300

    add page titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,18 +12,31 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+      meta: {
+        title: 'Login Page',
+      },
     },
     {
       path: '/home',
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
+      meta: {
+        title: 'Home - Todo Page',
+      },
     },
     {
       path: '/cards',
       name: 'cards',
       component: () => import('@/views/CardsView.vue'),
+      meta: {
+        title: 'Responsive Cards',
+      },
     },
   ],
+})
+
+router.beforeEach(async (to) => {
+  document.title = to.meta.title
 })
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import AppLayout from '@/layouts/AppLayout.vue'
-import {FilePen,Trash2,Save} from "lucide-vue-next"
+import { FilePen, Trash2, Save } from 'lucide-vue-next'
 
 interface Task {
   text: string
@@ -34,14 +34,8 @@ const toggleEdit = (index: number) => {
     <h1 class="font-medium">Home - Todo List</h1>
     <div class="w-full mt-3">
       <form @submit.prevent="addTask" class="flex gap-2">
-        <Input
-          v-model="newTask"
-          type="text"
-          placeholder="Enter a task"
-        />
-        <Button type="submit" :disabled="!newTask.trim()">
-          Add
-        </Button>
+        <Input v-model="newTask" type="text" placeholder="Enter a task" />
+        <Button type="submit" :disabled="!newTask.trim()"> Add </Button>
       </form>
 
       <ul class="mt-4 divide-y divide-gray-200">
@@ -57,11 +51,21 @@ const toggleEdit = (index: number) => {
             :class="{ 'text-gray-400': !task.editing }"
           />
           <div class="flex gap-2 ml-3">
-            <Button variant="ghost" size="icon" @click="toggleEdit(index)" class="text-gray-500 hover:underline">
+            <Button
+              variant="ghost"
+              size="icon"
+              @click="toggleEdit(index)"
+              class="text-gray-500 hover:underline"
+            >
               <Save v-if="task.editing" />
               <FilePen v-else />
             </Button>
-            <Button variant="ghost" size="icon" @click="deleteTask(index)" class="text-rose-500 hover:text-rose-700">
+            <Button
+              variant="ghost"
+              size="icon"
+              @click="deleteTask(index)"
+              class="text-rose-500 hover:text-rose-700"
+            >
               <Trash2 />
             </Button>
           </div>
